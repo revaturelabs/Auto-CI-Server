c9218d1b258d961d7c9f7e9476358d43f758f782
Lawrence Dang <reblank.et@gmail.com>
2020-09-22 16:57:54 -0700

    Started git integration

--- initialization_service/src/main/java/com/revature/autoci/init/InitServlet.java
+++ initialization_service/src/main/java/com/revature/autoci/init/InitServlet.java
@@ -1,6 +1,8 @@
 package com.revature.autoci.init;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -8,6 +10,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.google.gson.Gson;
+
+import org.apache.commons.io.FileUtils;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
 
 public class InitServlet extends HttpServlet 
 {
@@ -21,29 +28,33 @@ public class InitServlet extends HttpServlet
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        // TODO Auto-generated method stub
         // Load in JSON message
         Gson gson = new Gson();
         JSONRequest data = gson.fromJson(req.getReader(), JSONRequest.class);
         boolean success = false;
-        // create temp directory
-        
-        try
+        Path tempPath = Files.createTempDirectory("test");
+        System.out.println(tempPath.toAbsolutePath().toString());
+        try(LocalGitRepo git = new LocalGitRepo(data.getGithubURL(), tempPath, token))
         {
-
+            // create temp directory
             if(data.isMaven())
             {
                 // Call Maven generator
-                success = true;
             }
             else // Is node
             {
                 // Call Node generator
-                success = true;
             }
 
-            // git 
-
+            // git init, commit, push
+            git.addAndCommitAll();
+            git.pushToRemote();
+            success = true;
+        }
+        finally
+        {
+            // delete temp directory
+            FileUtils.deleteDirectory(tempPath.toFile());
         }
         
         if(success)
---
+++ initialization_service/src/main/java/com/revature/autoci/init/LocalGitRepo.java
@@ -0,0 +1,79 @@
+package com.revature.autoci.init;
+
+import java.nio.file.Path;
+
+import org.eclipse.jgit.api.AddCommand;
+import org.eclipse.jgit.api.CloneCommand;
+import org.eclipse.jgit.api.CommitCommand;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.PushCommand;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.transport.CredentialsProvider;
+import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
+
+public class LocalGitRepo implements AutoCloseable{
+    private String uri;
+    private Path cloneDir;
+    private CredentialsProvider credentials;
+    private Git repo;
+    public LocalGitRepo(String URI, Path cloneDir, String token)
+    {
+        uri = URI;
+        this.cloneDir = cloneDir;
+        credentials = new UsernamePasswordCredentialsProvider(token, "");
+        repo = cloneRepo();
+    }
+
+    private Git cloneRepo()
+    {
+        CloneCommand cloneCmd = Git.cloneRepository();
+        cloneCmd.setURI(uri);
+        cloneCmd.setCredentialsProvider(credentials);
+        cloneCmd.setDirectory(cloneDir.toFile());
+        Git repo = null;
+        try
+        {
+            repo = cloneCmd.call();
+        }
+        catch(GitAPIException e)
+        {
+            e.printStackTrace();
+        }
+        return repo;
+    }
+
+    public void addAndCommitAll()
+    {
+        AddCommand addCmd = repo.add();
+        addCmd.addFilepattern(".");
+        try {
+            addCmd.call();
+        } catch (GitAPIException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        CommitCommand commitCmd = repo.commit();
+        commitCmd.setAuthor("Auto-CI", "<>");
+        commitCmd.setCommitter("Auto-CI", "<>");
+        commitCmd.setMessage("Setting up a new project");
+        try {
+            commitCmd.call();
+        } catch (GitAPIException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    public void pushToRemote()
+    {
+        PushCommand pushCmd = repo.push();
+        pushCmd.setRemote(uri);
+        pushCmd.setCredentialsProvider(credentials);
+    }
+
+    public void close()
+    {
+        repo.getRepository().close();
+        repo.close();
+    }
+}
