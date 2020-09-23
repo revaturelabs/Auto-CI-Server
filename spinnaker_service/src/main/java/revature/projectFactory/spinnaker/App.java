5ed3e39d7fc106c2869fe7bceefd706f2537ae71
Reese Benson <ReeseABenson@gmail.com>
2020-09-23 12:29:33 -0700

    [task] - init tomcat server for spinnaker

---
+++ spinnaker_service/src/main/java/revature/projectFactory/spinnaker/App.java
@@ -0,0 +1,30 @@
+package revature.projectFactory.spinnaker;
+
+import java.io.File;
+
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.startup.Tomcat;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Tomcat server = new Tomcat();
+        server.setPort(8080);
+        server.setBaseDir(new File("target/tomcat").getAbsolutePath());
+        server.getConnector();
+        server.addWebapp("", new File("src/main/static").getAbsolutePath());
+        try {
+            server.start();
+            server.getServer().await();
+        } catch (LifecycleException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            System.out.println();
+        }
+    }
+}
