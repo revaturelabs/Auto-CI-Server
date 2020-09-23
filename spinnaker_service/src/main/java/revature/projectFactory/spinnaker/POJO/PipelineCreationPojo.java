5ec99ef8705c4d683964500c5df6d6713f6d5420
Reese Benson <ReeseABenson@gmail.com>
2020-09-23 13:53:38 -0700

    Inital pipeline CreationService

---
+++ spinnaker_service/src/main/java/revature/projectFactory/spinnaker/POJO/PipelineCreationPojo.java
@@ -0,0 +1,13 @@
+package revature.projectFactory.spinnaker.POJO;
+
+public class PipelineCreationPojo {
+    private String jobName;
+
+    public String getJobName() {
+        return jobName;
+    }
+
+    public void setJobName(String jobName) {
+        this.jobName = jobName;
+    }
+}
---
+++ spinnaker_service/src/main/java/revature/projectFactory/spinnaker/spinnakerServices/IPipeLineCreation.java
@@ -0,0 +1,7 @@
+package revature.projectFactory.spinnaker.spinnakerServices;
+
+import revature.projectFactory.spinnaker.POJO.PipelineCreationPojo;
+
+public interface IPipeLineCreation {
+    public boolean create(PipelineCreationPojo pipline);
+}
