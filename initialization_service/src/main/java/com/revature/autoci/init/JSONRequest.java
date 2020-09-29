d08a51d683f08adc37294f8dbbd58a5114975274
Lawrence Dang <lawrend1@uci.edu>
2020-09-29 08:43:47 -0700

    Refactor request format

--- initialization_service/src/main/java/com/revature/autoci/init/JSONRequest.java
+++ initialization_service/src/main/java/com/revature/autoci/init/JSONRequest.java
@@ -6,44 +6,64 @@ import java.util.Map;
 
 public class JSONRequest {
     private String githubUsername;
+    private String githubURL;
+    private String ide;
     private boolean isMaven;
     private boolean generateGHActions;
-    private String githubURL;
-    private Map<String, String> metadata;
-    private List<Map<String,String>> dependencies;
-    public JSONRequest(String githubUsername, boolean isMaven, boolean generateGHActions, String githubURL, Map<String, String> metadata, List<Map<String, String>> dependencies)
-    {
+    private MavenJSON mavenData;
+    private NpmJSON npmData;
+
+    public JSONRequest(String githubUsername, String githubURL, String ide, boolean isMaven, boolean generateGHActions,
+            MavenJSON mavenData) {
         this.githubUsername = githubUsername;
-        this.isMaven = isMaven;
         this.githubURL = githubURL;
-        this.metadata = metadata;
-        this.dependencies = dependencies;
+        this.ide = ide;
+        this.isMaven = isMaven;
+        this.generateGHActions = generateGHActions;
+        this.mavenData = mavenData;
+        this.npmData = null;;
+    }
+
+    public JSONRequest(String githubUsername, String githubURL, String ide, boolean isMaven, boolean generateGHActions,
+            NpmJSON npmData) {
+        this.githubUsername = githubUsername;
+        this.githubURL = githubURL;
+        this.ide = ide;
+        this.isMaven = isMaven;
+        this.generateGHActions = generateGHActions;
+        this.mavenData = null;
+        this.npmData = npmData;
     }
 
     public String getGithubUsername() {
         return githubUsername;
     }
 
-    public boolean isMaven() {
-        return isMaven;
-    }
-
     public String getGithubURL() {
         return githubURL;
     }
 
-    public List<Map<String, String>> getDependencies() {
-        return dependencies;
+    public String getIDE() {
+        return ide;
     }
 
-    public String getMetadataValue(String key)
-    {
-        return metadata.get(key);
+    public boolean isMaven() {
+        return isMaven;
     }
 
-    public boolean shouldGenerateGHActions()
-    {
+    public boolean shouldGenerateGHActions() {
         return generateGHActions;
     }
+
+    public MavenJSON getMavenData() {
+        return mavenData;
+    }
+
+    public NpmJSON getNpmData() {
+        return npmData;
+    }
+
+    
+
     
 }
