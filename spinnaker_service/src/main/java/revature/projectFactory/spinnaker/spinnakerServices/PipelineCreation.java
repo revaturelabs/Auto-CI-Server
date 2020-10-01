0ec911c40b906b93228b27d3a8e5410050bc041b
Douglas Liu <you@example.com>
2020-10-01 11:16:43 -0700

    cancelled logger

--- spinnaker_service/src/main/java/revature/projectFactory/spinnaker/spinnakerServices/PipelineCreation.java
+++ spinnaker_service/src/main/java/revature/projectFactory/spinnaker/spinnakerServices/PipelineCreation.java
@@ -9,20 +9,16 @@ import java.net.URL;
 
 import revature.projectFactory.spinnaker.POJO.PipelineCreationPojo;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 public class PipelineCreation implements IPipeLineCreation {
     private final String pipelineCreateEndpoint;
-    private final Logger log = LoggerFactory.getLogger(this.getClass());
 
-    PipelineCreation(String spinnakerURI){
-        pipelineCreateEndpoint = spinnakerURI+"/pipelines";
+    PipelineCreation(String spinnakerURI) {
+        pipelineCreateEndpoint = spinnakerURI + "/pipelines";
     }
-    
+
     @Override
     public boolean create(PipelineCreationPojo pipline) {
-        try{
+        try {
 
             URL url = new URL(pipelineCreateEndpoint);
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
@@ -31,25 +27,22 @@ public class PipelineCreation implements IPipeLineCreation {
             connection.setRequestProperty("Accept", "application/json");
             connection.setDoOutput(true);
             String jsonInputString = "{ someJson: \"change me\"}";
-            try(OutputStream os = connection.getOutputStream()) {
+            try (OutputStream os = connection.getOutputStream()) {
                 byte[] input = jsonInputString.getBytes("utf-8");
                 os.write(input, 0, input.length);
-                log.info("Spinnaker connection output stream ran in pipeline creation");		
             }
             StringBuilder responseBody = new StringBuilder();
-            BufferedReader reader = new BufferedReader( new InputStreamReader(connection.getInputStream()));
-            while(reader.ready()){
+            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            while (reader.ready()) {
                 responseBody.append(reader.readLine() + "\n");
             }
             System.out.println("Response:" + connection.getResponseCode() + "\n" + responseBody);
             reader.close();
-        }catch(IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
-            log.error("Spinnaker Get pipeline creation connection failed", e);
         }
-        log.info("Spinnaker pipeline creation connection is false");
         return false;
 
     }
-    
+
 }
