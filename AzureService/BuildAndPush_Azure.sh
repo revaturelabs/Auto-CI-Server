5989c8a0b367ce8fa5571f3d757592eb25b8f2c5
Alxl <44034025+ItsAlxl@users.noreply.github.com>
2020-10-13 23:06:02 -0500

    Use ECR instead of Docker Hub

--- AzureService/BuildAndPush_Azure.sh
+++ AzureService/BuildAndPush_Azure.sh
@@ -1,5 +1,4 @@
 mvn clean package
-echo "Enter Docker Hub username"
-read username
-docker build -t $username/autoci-azure:latest $(dirname $0)
-docker push $username/autoci-azure:latest
+docker build -t autoci-azure .
+docker tag autoci-azure:latest 855430746673.dkr.ecr.us-east-1.amazonaws.com/autoci-azure:latest
+docker push 855430746673.dkr.ecr.us-east-1.amazonaws.com/autoci-azure:latest
